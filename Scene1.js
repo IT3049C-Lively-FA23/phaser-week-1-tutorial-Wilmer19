2d72e55ba921e2452acba65b119ba706f3465216
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-10-17 18:21:23 -0400

    Added a preload function for the background image

--- Scene1.js
+++ Scene1.js
@@ -3,6 +3,10 @@ class Scene1 extends Phaser.Scene {
         super("bootGame");
     }
 
+    preload(){
+        this.load.image("background", "Assets/Images/background.png");
+    }
+
     create(){
         this.add.text(20, 20, "Loading game...");
         this.scene.start("playGame");

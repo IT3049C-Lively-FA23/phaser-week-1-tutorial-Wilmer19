f711311c74a988f1fd99b120e587850c38aa7907
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-10-17 18:23:16 -0400

    Created a preload function for the three ships

--- Scene1.js
+++ Scene1.js
@@ -5,6 +5,9 @@ class Scene1 extends Phaser.Scene {
 
     preload(){
         this.load.image("background", "Assets/Images/background.png");
+        this.load.image("background", "Assets/Images/ship.png");
+        this.load.image("background", "Assets/Images/ship2.png");
+        this.load.image("background", "Assets/Images/ship3.png");
     }
 
     create(){

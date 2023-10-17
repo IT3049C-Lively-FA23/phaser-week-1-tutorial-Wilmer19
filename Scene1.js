cf5c03effb1993fc25aee6fdd688f14b4cc42932
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-10-17 19:07:44 -0400

    Added the spritesheet function to the images

--- Scene1.js
+++ Scene1.js
@@ -5,9 +5,22 @@ class Scene1 extends Phaser.Scene {
 
     preload(){
         this.load.image("background", "Assets/Images/background.png");
-        this.load.image("ship", "Assets/Images/ship.png");
-        this.load.image("ship2", "Assets/Images/ship2.png");
-        this.load.image("ship3", "Assets/Images/ship3.png");
+        this.load.spritesheet("ship", "Assets/Spritesheets/ship.png",{
+            frameWidth: 16,
+            frameHeight: 16
+        });
+        this.load.spritesheet("ship2", "Assets/Spritesheets/ship2.png",{
+            frameWidth: 32,
+            frameHeight: 16
+        });
+        this.load.spritesheet("ship3", "Assets/Spritesheets/ship3.png",{
+            frameWidth: 32,
+            frameHeight: 32
+        });
+        this.load.spritesheet("explosion", "Assets/Spritesheets/explosion.png",{
+            frameWidth: 16,
+            frameHeight: 16
+        });
     }
 
     create(){

f98151d27acca2c188d292cdd1a8ef7e717e1e12
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-10-18 19:27:26 -0400

    Added beam image to canvas and animations

--- Scene1.js
+++ Scene1.js
@@ -29,6 +29,10 @@ class Scene1 extends Phaser.Scene {
             frameWidth: 16,
             frameHeight: 24
         });
+        this.load.spritesheet("beam", "Assets/Spritesheets/beam.png",{
+            frameWidth: 16,
+            frameHeight: 16
+        });
     }
 
     create(){
@@ -81,6 +85,12 @@ class Scene1 extends Phaser.Scene {
         this.anims.create({
             key: "thrust",
             frames: this.anims.generateFrameNumbers("player"),
+            frameRate: 20,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "beam_anim",
+            frames: this.anims.generateFrameNumbers("beam"),
             frameRate: 20,
             repeat: -1
         });
